77b3960ba896a41db6eeea7c129801a4b000caac
soumya saleena <soumya.ugargol@gmail.com>
2022-06-16 19:06:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/biggernumber/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.biggernumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+   
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnleft:Int = btnleft.text.toString().toInt()
+        btnleft.setOnClickListner {
+            
+        }
+
+
+
+    }
+}
